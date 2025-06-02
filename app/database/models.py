8f2298a5588d363d3c07fc88e3d7735790cd4103
AleIgoKha <aleigokha@gmail.com>
2025-06-02 19:51:23 +0300

    added a new column in orders table finished_datetime

--- app/database/models.py
+++ app/database/models.py
@@ -50,12 +50,13 @@ class Order(Base):
     issue_datetime: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
     delivery_price: Mapped[Decimal | None] = mapped_column(Numeric(7, 2), nullable=True)
     order_note: Mapped[str | None] = mapped_column(String, nullable=True)
-    order_text: Mapped[str | None] = mapped_column(String, nullable=True)
+    order_text: Mapped[str | None] = mapped_column(String, nullable=True) # не использовали
     order_disc: Mapped[int] = mapped_column(Integer, default=0)
     order_completed: Mapped[bool] = mapped_column(Boolean, default=False)
-    order_issued: Mapped[bool] = mapped_column(Boolean, default=False, server_default=text("FALSE"))
-    order_source: Mapped[str | None] = mapped_column(String, nullable=True)
-    creation_datetime: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    order_issued: Mapped[bool] = mapped_column(Boolean, default=False, server_default=text("FALSE")) # не использовали
+    order_source: Mapped[str | None] = mapped_column(String, nullable=True) # не использовали
+    creation_datetime: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False) # не использовали
+    finished_datetime: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
 
     session: Mapped["Session"] = relationship(back_populates="orders")
     
