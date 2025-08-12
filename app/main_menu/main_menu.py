9145706c9a5d520aafa819bb320c432613565e9f
AleIgoKha <aleigokha@gmail.com>
2025-08-12 16:46:20 +0300

    upgraded the ability to delete messages

--- app/main_menu/main_menu.py
+++ app/main_menu/main_menu.py
@@ -20,20 +20,27 @@ class Admin(Filter):
 async def start_handler(message: Message, state: FSMContext, bot: Bot):
     await state.clear()
     await message.delete()
-    sent_message = await message.answer(text='🏠 <b>ГЛАВНОЕ МЕНЮ</b>', reply_markup=kb.main_menu, parse_mode='HTML')
-    # Удаляем все другие сообщения, кроме последнего с меню пока не будет 5 сообщений подряд, которые уже удалены
-    message_id = sent_message.message_id
-    chat_id = sent_message.chat.id
-    for id in range(message_id - 1, 0, -1):
+    
+    # удаляем все id всех сообщений в чате
+    redis = bot.redis
+    chat_id = message.chat.id
+    key = f"chat:{chat_id}:messages"
+    message_ids = await redis.lrange(key, 0, -1)  # Get all stored message IDs
+
+    for msg_id in message_ids:
         try:
-            bad_tries = 0
-            await bot.delete_message(chat_id=chat_id, message_id=id)
-        except TelegramBadRequest:
-            bad_tries += 1
-            if bad_tries <= 5:
-                continue
-            else:
-                break
+            await bot.delete_message(chat_id, int(msg_id))
+        except Exception:
+            pass  # Ignore errors (e.g., message already deleted)
+
+    await redis.delete(key)  # Clear the stored list
+    await redis.close()
+    
+    # печатаем сообщение и сохраняем его id
+    sent_message = await message.answer(text='🏠 <b>ГЛАВНОЕ МЕНЮ</b>', reply_markup=kb.main_menu, parse_mode='HTML')
+    message_id = sent_message.message_id
+    await redis.rpush(f"chat:{chat_id}:messages", message_id)
+    await redis.close()
 
 # Возвращение через колбэк в главное меню или при вызове функции
 @main_menu.callback_query(F.data == 'main:menu')
